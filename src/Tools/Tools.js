a7aa0f1e1380523a145d4d6bc690cd020f040ebd
madhank1800 <madhankumar1800@gmail.com>
2022-12-10 23:28:47 +0530

    tools page

---
+++ src/Tools/Tools.js
@@ -0,0 +1,9 @@
+import React from "react";
+//import Dashboard from "../Dashboard/Dashboard";
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+const Tools = () => {
+  const navigate = useNavigate();
+  return <></>;
+};
+export default Tools;
